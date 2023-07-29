9e2046b0ae4aff057b1ebd05257cd925bb0f5674
mint723 <114153768+mint723@users.noreply.github.com>
2023-07-30 00:08:58 +0900

    build

---
+++ src/main/java/com/itmanagement/hsorder/HsorderApplication.java
@@ -0,0 +1,13 @@
+package com.itmanagement.hsorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsorderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itmanagement/hsorder/HsorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmanagement.hsorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsorderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
